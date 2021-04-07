9372bf66e5e0ac641988b1b839cbdb9308c6fdc1
Administrator <2431324403@qq.com>
2021-04-07 15:51:58 +0800

    cancel notes

--- src/main/java/com/yhtx/easy/annotation/Result.java
+++ src/main/java/com/yhtx/easy/annotation/Result.java
@@ -2,11 +2,6 @@ package com.yhtx.easy.annotation;
 
 import java.lang.annotation.*;
 
-/**
-* @Description:
-* @Author: zxy
-* @Date: 15:06
-*/
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE,ElementType.METHOD})
 @Documented
